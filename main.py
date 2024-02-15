4af7f5639a588447f676e731b95eff96fa040fd3
Levi Powis <lpowis97@gmail.com>
2024-02-15 14:56:38 -0500

    add get_sorted_character_list function and added a report output section to main()

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_letters = get_num_letters(text)
+    sorted_character_list = get_sorted_character_list(num_letters)
+    
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(num_letters)
+    print("")
+    for character in sorted_character_list:
+        letter = character["letter"]
+        num = character["num"]
+        print(f"The {letter} character was found {num} times")
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -19,9 +27,19 @@ def get_num_letters(text):
         letter_counts[letter] += 1
     return letter_counts
 
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_sorted_character_list(num_letters):
+    character_list = []
+    for key, value in num_letters.items():
+        if key.isalpha():
+            character_list.append({"letter": key, "num": value})
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
+
+def sort_on(character_list):
+    return character_list["num"]
 
 main()
