7440c77a5f18a0bfc92851de56f45af949d50365
Levi Powis <lpowis97@gmail.com>
2024-02-15 13:54:13 -0500

    add get_num_letters() function that returns a dictionary of characters and how many times they appear in the book

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_letters = get_num_letters(text)
     print(f"{num_words} words found in the document")
+    print(num_letters)
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_letters(text):
+    letter_counts = {}
+    for i in range(0, len(text)):
+        letter = text[i].lower()
+        if letter not in letter_counts:
+            letter_counts[letter] = 0
+        letter_counts[letter] += 1
+    return letter_counts
+
 
 def get_book_text(path):
     with open(path) as f:
